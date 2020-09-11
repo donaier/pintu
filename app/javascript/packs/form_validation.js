f3305a6d511fdb1ca64ccc829bacc3af0503e849
donat <donat.baier@gmail.com>
2020-09-11 15:42:47 +0200

    basic validation

---
+++ app/javascript/packs/form_validation.js
@@ -0,0 +1,15 @@
+document.addEventListener("turbolinks:load", function() {
+  'use strict';
+  var forms = document.getElementsByClassName('needs-validation');
+  // Loop over them and prevent submission
+  var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      console.log(form.checkValidity());
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})
