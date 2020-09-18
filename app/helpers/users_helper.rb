a5d76fe4bb1591ab25f6d834abb300472a892bf5
donat <donat.baier@gmail.com>
2020-09-18 11:25:46 +0200

    betterify user mgmt

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def index_roles(user)
+    if user.has_role? :bambi
+      I18n.t("role.bambi")
+    else
+      user.roles.map{ |r| I18n.t("role.#{r.name}") }.join(' | ')
+    end
+  end
 end
